c43d2bb32698a65e5f804b2d8d310cba2209903f
manishsinghmodi <manishsahu43962@gmail.com>
2025-01-21 01:58:25 +0530

    vite build added

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base : '/Marketing-Website/',
   plugins: [react()],
+
+"scripts": {
+  "dev": "vite",
+  "build": "vite build",
+  "preview": "vite preview"
+}
 })
