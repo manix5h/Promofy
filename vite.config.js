d76887d5679774a974e3320396c0f67c8cc5b5c7
manishsinghmodi <manishsahu43962@gmail.com>
2025-01-22 01:24:17 +0530

    again deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : '/Marketing-Website/',
   plugins: [react()],
-  base : "/promofy/"
- 
+  
+
+
 })
